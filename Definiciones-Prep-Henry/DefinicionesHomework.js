04d8e353e0026f747c4cd554f695889a32c43bc9
GabrielOviedo <oviedongo@gmail.com>
2021-04-13 18:47:06 -0300

    Modificado

---
+++ Definiciones-Prep-Henry/DefinicionesHomework.js
@@ -0,0 +1,23 @@
+Definiciones Curso.Prep.Henry "JSI"
+
+Variable: Elemento que se usa para almacenar y hacer referencia a un valor.  Las variables permiten crear 'Programas Genericos', que funcionan siempre igual, indispensablemente de los valores utilizados.
+
+String: Es una cadena de caracteres, contiene letras o numeros y se representa entre comillas simpres '' o dobles "".
+
+Funciones: Es un conjunto de instrucciones que se agrupan para realizar una tarea concreta y puede reutilizarse facilmente.
+
+If: Evalua una condicion y en caso que se cumplan las condiciones, se ejecuta el contenido entre llaves "{}". Si no se aplica dichas llaves solo se ejecutara el contenido hasta el proximo ";".
+
+Valores Booleanos: Son datos que responden valores booleanos, es decir "True" o "False"
+
+
+
+Definiciones Curso.Prep.Henry "JSII"
+
+For: Crea un bucle para hacer y ejecutar comandos repetitivos, de esta forma ahorra tiempo. Cuenta de una "Expesion Inicial", "Condicion", "Expresion Final" y "Sentencia"
+
+"&&", "||" y "!": Son operadores logicos.
+-&&: AND : y
+-||: OR : o
+!: NOT : !
+Comparan Valores Booleanos y responde con "True" o "False".
